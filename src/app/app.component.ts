5ea33f378c9de7f7907c5ba268cdf7f5d4f5ac33
adey-px <aosteph@gmail.com>
2023-10-03 23:53:17 -0500

    Removed unnecessary content from files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+// Root Component - loads all other components
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// Export Root Component to app.module
 export class AppComponent {
   title = 'andreivideostreamin';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
+// Root Module - load all other modules
 @NgModule({
   declarations: [
     AppComponent
@@ -13,4 +13,6 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
+// Export Root Module to main.ts
 export class AppModule { }
